6859128da45fc1070ec721167783f8244dc8a981
Guilherme Krieck <guilherme.krieck@gmail.com>
2024-09-10 10:12:11 -0300

    teste 3
    implementada a lógica para o terceiro teste

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -12,6 +12,9 @@ class RecintosZoo {
         try {
             const ehAnimalValido = this.verificaAnimal(animal);
             const ehQuantidadeValida = this.verificaQuantidade(quantidade);
+            if(ehAnimalValido && ehQuantidadeValida){
+                resultado.recintoViaveis = this.encontraRecintosViaveis(animal, quantidade);
+            }
         }catch (erro){
             resultado.erro = erro.message;
             resultado.recintoViaveis = null;
@@ -38,6 +41,38 @@ class RecintosZoo {
         }
 
         throw new Error("Quantidade inválida");
+    }
+
+    encontraRecintosViaveis(animal, quantidade){
+        const dadosDoAnimal = animaisAceitos.find((animalAceito) => animalAceito.especie === animal);
+        const ambientesFavoraveisDoAnimal = dadosDoAnimal.ambientesFavoraveis;
+
+        const recintoViaveis = recintos.filter((recinto) => {
+
+            const biomaEhFavoravel = recinto.bioma.some((bioma) => ambientesFavoraveisDoAnimal.includes(bioma));
+
+            const temEspacoDisponivel = this.verificaEspaçoDisponivelRecinto(dadosDoAnimal, recinto, quantidade);
+
+            return biomaEhFavoravel && temEspacoDisponivel
+        })
+
+        if (recintoViaveis.length > 0){
+            return console.log(recintoViaveis);
+        }
+
+        throw new Error("Não há recinto viável");
+    }
+
+    verificaEspaçoDisponivelRecinto(dadosDoAnimal, recinto, quantidade){
+        let tamanhoOcupado = recinto.tamanhoOcupado;
+
+        tamanhoOcupado += dadosDoAnimal.tamanho * quantidade;
+
+        if(recinto.animaisResidentes.length > 0 && !recinto.animaisResidentes.every(residente => residente === dadosDoAnimal.especie)){
+            tamanhoOcupado += 1;
+        }
+
+        return recinto.tamanho >= tamanhoOcupado;
     }
 
 }
