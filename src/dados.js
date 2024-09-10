c5444c1c679a6ed99f3db3d03c1de7aacde7e29f
Guilherme Krieck <guilherme.krieck@gmail.com>
2024-09-09 22:13:26 -0300

    Teste 1
    criado o arquivo dados.js com objetos e desenvolvida a lógica para passar no primeiro teste.

---
+++ src/dados.js
@@ -0,0 +1,76 @@
+export const recintos = [
+    {
+        name: "Recinto1",
+        bioma: ["savana"],
+        tamanho: 10,
+        tamanhoOcupado: 3,
+        animaisResidentes: ["MACACO", "MACACO", "MACACO"]
+    },
+    {
+        name: "Recinto2",
+        bioma: ["floresta"],
+        tamanho: 5,
+        tamanhoOcupado: 0,
+        animaisResidentes: []
+    },
+    {
+        name: "Recinto3",
+        bioma: ["savana","rio"],
+        tamanho: 7,
+        tamanhoOcupado: 2,
+        animaisResidentes: ["GAZELA"]
+    },
+    {
+        name: "Recinto4",
+        bioma: ["rio"],
+        tamanho: 8,
+        tamanhoOcupado: 0,
+        animaisResidentes: []
+    },
+    {
+        name: "Recinto5",
+        bioma: ["savana"],
+        tamanho: 9,
+        tamanhoOcupado: 3,
+        animaisResidentes: ["LEAO"]
+    }
+]
+
+export const animaisAceitos = [
+    {
+        especie: "LEAO",
+        tamanho: 3,
+        ambientesFavoraveis:["savana"],
+        caracteristica:"carnivoro"
+    },
+    {
+        especie: "LEOPARDO",
+        tamanho: 2,
+        ambientesFavoraveis:["savana"],
+        caracteristica:"carnivoro"
+    },
+    {
+        especie: "CROCODILO",
+        tamanho: 3,
+        ambientesFavoraveis:["rio"],
+        caracteristica:"carnivoro"
+    },
+    {
+        especie: "MACACO",
+        tamanho: 1,
+        ambientesFavoraveis:["savana", "floresta"],
+        caracteristica:"onivoros"
+    },
+    {
+        especie: "GAZELA",
+        tamanho: 2,
+        ambientesFavoraveis:["savana"],
+        caracteristica:"herbivoro"
+    },
+    {
+        especie: "HIPOPOTAMO",
+        tamanho: 4,
+        ambientesFavoraveis:["savana", "rio"],
+        caracteristica:"herbivoro"
+    }
+]
--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,6 +1,34 @@
+import{recintos, animaisAceitos} from "./dados.js"
+
 class RecintosZoo {
 
     analisaRecintos(animal, quantidade) {
+
+        let resultado = {
+            erro: false,
+            recintoViaveis: null
+        };
+
+        try {
+            const ehAnimalValido = this.verificaAnimal(animal);
+        }catch (erro){
+            resultado.erro = erro.message;
+            resultado.recintoViaveis = null;
+
+        };
+
+        return resultado;
+
+    }
+
+    verificaAnimal(novoAnimal){
+        const animalFiltrados = animaisAceitos.filter((animal) => animal.especie === novoAnimal);
+
+        if(animalFiltrados.length === 1) {
+            return true;
+        }
+
+        throw new Error("Animal inválido");
     }
 
 }
